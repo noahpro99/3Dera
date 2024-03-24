cd124ade9a07ca1a17b27457a5d32edc37085aeb
ayk <jungmink623@gmail.com>
2024-03-24 02:21:34 -0400

    range only

--- frontend/museum/src/home.tsx
+++ frontend/museum/src/home.tsx
@@ -75,7 +75,7 @@ export default function Example() {
 
         <div className="lg:pl-72">
           <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
-            <div className="text-2xl font-bold">Year Range</div>
+            <div className="text-2xl font-bold">Range</div>
             <div className="p-4 bg-white w-full gap-4 flex flex-col">
               <ReactSlider
                 className="horizontal-slider"
